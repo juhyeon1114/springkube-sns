b71a99f8438e6361df8f5b95af4c5973735a655a
juhyeon <juhyeon1114@gmail.com>
2025-01-21 15:40:33 +0900

    init

---
+++ src/main/java/study/springkubesns/SpringkubeSnsApplication.java
@@ -0,0 +1,13 @@
+package study.springkubesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkubeSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkubeSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/springkubesns/SpringkubeSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springkubesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkubeSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
