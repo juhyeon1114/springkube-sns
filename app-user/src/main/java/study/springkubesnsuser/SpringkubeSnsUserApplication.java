cc0b1a9bd506d504c1a693794fea10070b0f0e4a
juhyeon <juhyeon1114@gmail.com>
2025-01-30 22:31:12 +0900

    User 서버 Init

---
+++ app-user/src/main/java/study/springkubesnsuser/SpringkubeSnsUserApplication.java
@@ -0,0 +1,13 @@
+package study.springkubesnsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkubeSnsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkubeSnsUserApplication.class, args);
+    }
+
+}
