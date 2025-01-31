a7a13f4bebb3f479c881133a16ef4f0664569879
juhyeon <juhyeon1114@gmail.com>
2025-01-31 22:06:42 +0900

    image server 초기 세팅

---
+++ app-image/src/main/java/study/springkubesnsimage/SpringkubeSnsImageApplication.java
@@ -0,0 +1,13 @@
+package study.springkubesnsimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkubeSnsImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkubeSnsImageApplication.class, args);
+    }
+
+}
